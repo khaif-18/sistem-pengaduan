7fd1bb33ce35f31ca9284023d0f2dcd25744fb6e
Khaif <khalifaherian@gmail.com>
2024-11-04 10:36:24 +0700

    [fix] try change promise resolve

--- src/app/kir/page.tsx
+++ src/app/kir/page.tsx
@@ -2,11 +2,10 @@
 
 import dynamic from 'next/dynamic';
 import { pdfjs } from 'react-pdf';
-import KirComp from './kirComp';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `/pdf.worker.min.mjs`;
 
-const DynamicKirComp = dynamic(() => Promise.resolve(KirComp), { ssr: false });
+const DynamicKirComp = dynamic(() => import('./kirComp'), { ssr: false });
 
 export default function Kir() {
   return <DynamicKirComp />;
