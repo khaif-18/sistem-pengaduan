1e30a43dd299767d66d0c4d5b164775cc5d9c296
Khaif <khalifaherian@gmail.com>
2024-11-06 10:35:23 +0700

    [chore] change header res disable cache control

--- src/app/api/complaint/get/route.ts
+++ src/app/api/complaint/get/route.ts
@@ -4,31 +4,42 @@ import pengaduan from "@/server/models/form";
 import { NextResponse } from "next/server";
 
 export async function GET() {
-  try {
-    await connectDB();
+    try {
+        await connectDB();
 
-    const dataPengaduan = await pengaduan.find({}).sort({ _id: -1 }).exec();
+        const dataPengaduan = await pengaduan.find({}).sort({ _id: -1 }).exec();
 
-    const responseData = dataPengaduan.map((record) => {
-      return {
-        ...record.toObject(),
-        evidence: record.evidence
-          ? `data:image/jpeg;base64,${record.evidence.toString("base64")}`
-          : null,
-      };
-    });
+        const responseData = dataPengaduan.map((record) => {
+            return {
+                ...record.toObject(),
+                evidence: record.evidence
+                    ? `data:image/jpeg;base64,${record.evidence.toString(
+                          "base64"
+                      )}`
+                    : null,
+            };
+        });
 
-    return NextResponse.json(responseData);
-  } catch (error: unknown) {
-    console.error("Error:", error);
+        return NextResponse.json(responseData, {
+            status: 200,
+            headers: {
+                "Cache-Control": "public, s-maxage=0",
+                "CDN-Cache-Control": "public, s-maxage=0",
+                "Vercel-CDN-Cache-Control": "public, s-maxage=0",
+            },
+        });
+    } catch (error: unknown) {
+        console.error("Error:", error);
 
-    return NextResponse.json(
-      {
-        error:
-          "Gagal mengambil data pengaduan: " +
-          (error instanceof Error ? error.message : "kesalahan yang tidak diketahui"),
-      },
-      { status: 500 }
-    );
-  }
+        return NextResponse.json(
+            {
+                error:
+                    "Gagal mengambil data pengaduan: " +
+                    (error instanceof Error
+                        ? error.message
+                        : "kesalahan yang tidak diketahui"),
+            },
+            { status: 500 }
+        );
+    }
 }
